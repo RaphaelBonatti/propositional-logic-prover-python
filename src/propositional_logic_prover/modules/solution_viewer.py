2d734075854546a5114948ebb00a5037a05aa6a0
Raphael <raphael.bonatti@protonmail.com>
2025-02-09 00:06:09 +0100

    refactor: increase cohesion

--- src/propositional_logic_prover/modules/solution_viewer.py
+++ src/propositional_logic_prover/modules/solution_viewer.py
@@ -3,43 +3,46 @@ from anytree.exporter import UniqueDotExporter
 from modules.clause import Clause
 
 
-class SolutionViewer:
+class DerivationBuilder:
     def __init__(
         self, derivation_map: dict[Clause, tuple[Clause, Clause]]
     ) -> None:
         self._derivation_map: dict[Clause, tuple[Clause, Clause]] = (
             derivation_map
         )
-        self._node: Node = self._derive(frozenset())
 
-    def _clause_to_str(self, clause: Clause) -> str:
-        if len(clause) > 0:
-            return ", ".join(clause)
-
-        return "□"
-
-    def _derive(self, clause: Clause) -> Node:
+    def create_node(self, clause: Clause) -> Node:
         if clause in self._derivation_map.keys():
             clause1, clause2 = self._derivation_map[clause]
-            node_lhs = self._derive(clause1)
-            node_rhs = self._derive(clause2)
-            return Node(
-                self._clause_to_str(clause), children=[node_lhs, node_rhs]
-            )
+            node_lhs = self.create_node(clause1)
+            node_rhs = self.create_node(clause2)
+            return Node(clause_to_str(clause), children=[node_lhs, node_rhs])
 
-        return Node(self._clause_to_str(clause))
+        return Node(clause_to_str(clause))
 
-    def show_derivation(self) -> None:
-        for pre, _, node in RenderTree(self._node):
-            print("%s%s" % (pre, node.name))
+    def build(self) -> Node:
+        return self.create_node(frozenset())
 
-    def save_derivation_png(self, path: str) -> None:
-        # Turn graph bottom up and redirect edges back to parent
-        UniqueDotExporter(
-            self._node,
-            options=[
-                'rankdir="BT"',
-                'edge [dir="back"]',
-                'node [shape="box"]',
-            ],
-        ).to_picture(path)
+
+def clause_to_str(clause: Clause) -> str:
+    if len(clause) > 0:
+        return ", ".join(clause)
+
+    return "□"
+
+
+def show_derivation(node: Node) -> None:
+    for pre, _, node in RenderTree(node):
+        print("%s%s" % (pre, node.name))
+
+
+def save_derivation_png(node: Node, path: str) -> None:
+    # Turn graph bottom up and redirect edges back to parent
+    UniqueDotExporter(
+        node,
+        options=[
+            'rankdir="BT"',
+            'edge [dir="back"]',
+            'node [shape="box"]',
+        ],
+    ).to_picture(path)
